780d774c34e14a1979315c36cc287101f20f6aed
TheKaizenWolf <raul670saavedra@gmail.com>
2020-02-13 23:06:20 -0300

    update index

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const raul = { name: 'Raul', age: 100, cool: true };
+  res.send(req.query.name);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
