5cf38afb84f2569d7fc48dd1dab15e6f92f39cbc
TheKaizenWolf <raul670saavedra@gmail.com>
2020-02-15 16:11:49 -0300

    add flash

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -12,8 +11,12 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await new Store(req.body).save();
+
+  req.flash(
+    'success',
+    `Successfully Created ${store.name}. Would you like to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
   console.log('It worked!');
 };
