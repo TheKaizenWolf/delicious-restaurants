839e06479dc4be991cbce87435f972974015cbd1
TheKaizenWolf <raul670saavedra@gmail.com>
2020-02-19 19:05:08 -0300

    finish edit store and add lat and lng

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -29,5 +29,22 @@ exports.getStores = async (req, res) => {
 
 exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
-  res.json(store);
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    req.body,
+    { mew: true, runValidators: true }
+  ).exec();
+  req.flash(
+    'success',
+    `Succesfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store</a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
