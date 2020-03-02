b81e4a16a93b3923f456e22fe99d13991e4c469b
TheKaizenWolf <raul670saavedra@gmail.com>
2020-03-02 10:56:26 -0300

    add register page

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,6 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
 module.exports = router;
