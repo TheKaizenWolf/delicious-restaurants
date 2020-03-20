25adffc9b5204df530e4845cc88f85b4229b1135
TheKaizenWolf <raul670saavedra@gmail.com>
2020-03-20 17:50:36 -0300

    pushing user data to our api

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
 
+const User = mongoose.model('User');
 const Store = mongoose.model('Store');
 
 const multerOptions = {
@@ -149,4 +150,16 @@ exports.mapStores = async (req, res) => {
 
 exports.mapPage = (req, res) => {
   res.render('map', { title: 'Map' });
+};
+
+exports.heartStore = async (req, res) => {
+  const hearts = req.user.hearts.map(obj => obj.toString());
+  const operator = hearts.includes(req.params.id) ? '$pull' : '$addToSet';
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    { [operator]: { hearts: req.params.id } },
+    { new: true }
+  );
+  res.json(user);
 };
