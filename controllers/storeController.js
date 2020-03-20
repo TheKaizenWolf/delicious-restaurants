84ca3656fb81dd5477817b70fc73ac050436e077
TheKaizenWolf <raul670saavedra@gmail.com>
2020-03-20 19:03:54 -0300

    display hearted stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -94,9 +94,7 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug }).populate(
-    'author'
-  );
+  const store = await Store.findOne({ slug: req.params.slug });
   if (!store) {
     next();
   }
@@ -162,4 +160,11 @@ exports.heartStore = async (req, res) => {
     { new: true }
   );
   res.json(user);
+};
+
+exports.getHearts = async (req, res) => {
+  const stores = await Store.find({
+    _id: { $in: req.user.hearts },
+  });
+  res.render('stores', { title: 'Hearted Stores', stores });
 };
--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,11 @@ router.post(
   catchErrors(authController.update)
 );
 router.get('/map', storeController.mapPage);
-
+router.get(
+  '/hearts',
+  authController.isLoggedIn,
+  catchErrors(storeController.getHearts)
+);
 /* API */
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
