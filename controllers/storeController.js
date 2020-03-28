f319c4e7fdc3c48b5f918d68ba52394f226fd6bf
TheKaizenWolf <raul670saavedra@gmail.com>
2020-03-28 15:55:49 -0300

    finish pagination

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -53,9 +53,28 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-  const stores = await Store.find();
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = page * limit - limit;
+  const storesPromise = Store.find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' });
 
-  res.render('stores', { title: 'Stores', stores });
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+  if (!stores.length && skip) {
+    req.flash(
+      'info',
+      `Hey! You asked for page ${page}, but that does not exist. You are now at page ${pages} `
+    );
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+  res.render('stores', { title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
